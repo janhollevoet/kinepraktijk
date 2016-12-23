82b457d0e0cf471e638c056b1d3a03887fea13e5
Jan Hollevoet <jan.hollevoet@gmail.com>
2016-12-23 11:10:46 +0100

    Initiele versie voor kinepraktijk

---
+++ assets/js/main.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+    $('#menu-icon').click(function(){
+        $('nav.menu').addClass("toggle")
+    });
+    $('#menu-icon').click(function(){
+        $('nav.menu').addClass("toggle")
+    });
+    
+    
+    
+    
+});
